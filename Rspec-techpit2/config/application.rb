044fd916395ee44642266d26cb98043766796bd8
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-04 14:45:34 +0900

    [add]Language setting (japanese)

--- Rspec-techpit2/config/application.rb
+++ Rspec-techpit2/config/application.rb
@@ -31,5 +31,7 @@ module RspecTechpit2
     config.generators.system_tests = nil
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
   end
 end
